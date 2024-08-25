be5cd27128ce4d0acc1d78d2e39bb99bfd3ea55a
Marcel Putra <marcelino.ares.putra@gmail.com>
2024-08-25 13:24:33 +0800

    pre-jobs
    successfully implemented a boid manager monobehaviour

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -1,98 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    public List<Boid> neighbors = new List<Boid>();
+
     public Vector3 position;
     public Vector3 velocity;
     public Vector3 acceleration;
+    public Vector3 wanderTarget;
 
-    [SerializeField] WorldManager world;
-    [SerializeField] Config config;
-
-    [SerializeField] Material lineMaterial;
-    [SerializeField] Material trailMaterial;
-
-    private Vector3 wanderTarget = Vector3.zero;
-
-    private bool shouldDraw = false;
-
-    private void Awake()
+    public Boid(Vector3 position, Vector3 velocity, Vector3 acceleration, Vector3 wanderTarget)
     {
-        // setup components here
-        GetComponent<TrailRenderer>().material = trailMaterial;
-    }
-
-    private void Start()
-    {
-        // init position (important)
-        position = transform.position;
-    }
-
-    private void Update()
-    {
-        acceleration = Combine();
-        acceleration = Vector3.ClampMagnitude(acceleration, config.boidMaxAcceleration);
-
-        // apply acceleration
-        velocity = Vector3.ClampMagnitude(velocity + acceleration * Time.deltaTime, config.boidMaxVelocity);
-
-        // update position
-        position += velocity * Time.deltaTime;
-        transform.position = position;
-
-        shouldDraw = true;
-    }
-
-    private Vector3 Combine()
-    {
-        Vector3 result = Wander() * config.wanderWeight + avoidBounds();
-        return result.normalized;
-    }
-
-    private Vector3 Wander()
-    {
-        wanderTarget += new Vector3(
-            Random.Range(-1f, 1f),
-            Random.Range(-1f, 1f),
-            Random.Range(-1f, 1f)
-        ).normalized;
-
-        wanderTarget = Vector3.ClampMagnitude(wanderTarget, config.wanderRadius);
-
-        return wanderTarget.normalized;
-    }
-
-    private Vector3 avoidBounds()
-    {
-        Vector3 avoidanceVector = Vector3.zero;
-        float edgeDistance = config.boundSize * 0.3f;
-
-        avoidanceVector.x = position.x < -edgeDistance ? 1 : position.x > edgeDistance ? -1 : 0;
-        avoidanceVector.y = position.y < -edgeDistance ? 1 : position.y > edgeDistance ? -1 : 0;
-        avoidanceVector.z = position.z < -edgeDistance ? 1 : position.z > edgeDistance ? -1 : 0;
-
-        return avoidanceVector * 100f; // yes
-    }
-
-    private void OnRenderObject()
-    {
-        // ensure draw calls synced with update()
-        if (shouldDraw)
-        {
-            GL.PushMatrix();
-            GL.Begin(GL.LINES);
-            lineMaterial.SetPass(0);
-
-            GL.Color(Color.red); // change line color here
-            GL.Vertex(position);
-            GL.Vertex(position + velocity);
-
-            GL.End();
-            GL.PopMatrix();
-
-            shouldDraw = false; // reset flag
-        }
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.wanderTarget = wanderTarget;
     }
 }
--- Assets/Scripts/BoidManager.cs
+++ Assets/Scripts/BoidManager.cs
@@ -11,27 +11,124 @@ public class BoidManager : MonoBehaviour
 {
     [SerializeField] private Config config;
     [SerializeField] private GameObject boidPrefab;
-    private List<Boid> boids = new List<Boid>();
+    [SerializeField] private Material lineMaterial;
+
+    private List<GameObject> boids = new List<GameObject>();
+    //private List<Boid> boids = new List<Boid>();
+
+    private bool shouldDraw = false;
 
     private void Awake()
     {
         // spawn in boids
         for (int i = 0; i < config.boidsCount; i++)
         {
-            Vector3 spawnPosition = UnityEngine.Random.insideUnitSphere * (config.spawnRadius / 2);
+            Vector3 spawnPosition = UnityEngine.Random.insideUnitSphere * (config.spawnRadius / 1.5f);
             GameObject obj = Instantiate(boidPrefab, spawnPosition, Quaternion.identity);
             obj.gameObject.SetActive(true);
-            boids.Add(obj.GetComponent<Boid>());
+            boids.Add(obj);
+
+            Boid boid = obj.GetComponent<Boid>();
+            boid.position = spawnPosition;
+            boid.wanderTarget = Vector3.zero;
         }
     }
 
     private void Update()
     {
-        for(int i = 0; i < boids.Count; i++)
+        foreach(GameObject boidObj in boids)
         {
-            // implement individual boid behaviour here
-            // directly manipulate the vectors
+            Boid boid = boidObj.GetComponent<Boid>();
+
+            // update neighbors list
+            boid.neighbors = GetNeighbors(boidObj);
+
+            // calculate acceleration
+            Vector3 acceleration = Vector3.ClampMagnitude(Combine(boid), config.boidMaxAcceleration);
+            Vector3 velocity = Vector3.ClampMagnitude(boid.velocity + acceleration * Time.deltaTime, config.boidMaxVelocity);
+            Vector3 position = boid.position + velocity * Time.deltaTime;
+
+            // update boid component and transform
+            boid.velocity = velocity;
+            boid.acceleration = acceleration;
+            boid.position = position;
+            boidObj.transform.position = position;
+
+            shouldDraw = true;
         }
+    }
+
+    private List<Boid> GetNeighbors(GameObject targetBoidObj)
+    {
+        Boid targetBoid = targetBoidObj.GetComponent<Boid>();
+        List<Boid> neighbors = new List<Boid>();
+        float radiusSquared = Mathf.Pow(config.boidViewDistance, 2); // use squared radius for distance comparison
+
+        foreach (GameObject boidObj in boids)
+        {
+            if (boidObj != targetBoidObj) // Ignore the boid itself
+            {
+                Boid boid = boidObj.GetComponent<Boid>();
+                float distanceSquared = (boid.position - targetBoid.position).sqrMagnitude;
+
+                if (distanceSquared <= radiusSquared)
+                {
+                    neighbors.Add(boid);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    private Vector3 Combine(Boid boid)
+    {
+        return (Wander(boid) * config.wanderWeight + avoidBounds(boid)).normalized;
+    }
+
+    private Vector3 Wander(Boid boid)
+    {
+        boid.wanderTarget += new Vector3(
+            UnityEngine.Random.Range(-1f, 1f),
+            UnityEngine.Random.Range(-1f, 1f),
+            UnityEngine.Random.Range(-1f, 1f)
+        ).normalized;
+
+        return (Vector3.ClampMagnitude(boid.wanderTarget, config.wanderRadius)).normalized;
+    }
+
+    private Vector3 avoidBounds(Boid boid)
+    {
+        Vector3 avoidanceVector = Vector3.zero;
+        float edgeDistance = config.boundSize * 0.3f;
+
+        avoidanceVector.x = boid.position.x < -edgeDistance ? 1 : boid.position.x > edgeDistance ? -1 : 0;
+        avoidanceVector.y = boid.position.y < -edgeDistance ? 1 : boid.position.y > edgeDistance ? -1 : 0;
+        avoidanceVector.z = boid.position.z < -edgeDistance ? 1 : boid.position.z > edgeDistance ? -1 : 0;
+
+        return avoidanceVector * 100f; // yes
+    }
+
+    private void OnRenderObject()
+    {
+        // ensure draw calls are synced with update
+        if (!shouldDraw) return;
+
+        foreach(GameObject boidObj in boids)
+        {
+            GL.PushMatrix();
+            GL.Begin(GL.LINES);
+            lineMaterial.SetPass(0);
+
+            GL.Color(Color.red);
+            GL.Vertex(boidObj.transform.position);
+            GL.Vertex(boidObj.transform.position + boidObj.GetComponent<Boid>().velocity);
+
+            GL.End();
+            GL.PopMatrix();
+        }
+
+        shouldDraw = false;
     }
 }
 
@@ -41,6 +138,6 @@ public struct BoidJob : IJobParallelForTransform
 {
     public void Execute(int index, TransformAccess transform)
     {
-        throw new System.NotImplementedException();
+        // boid vector calculations and job stuff here
     }
 }
